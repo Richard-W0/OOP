aefa1d8e1b780b196dac55ad8657236565e5f6b5
Richard-W0 <rikuwong@hotmail.fi>
2025-02-03 20:23:21 +0200

    player.java

--- Week4/demo/src/main/java/main/App.java
+++ Week4/demo/src/main/java/main/App.java
@@ -4,10 +4,8 @@ package main;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
     }
 }
